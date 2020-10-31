abb626b91edcf90c59fc769ff502ffb66ae15a14
Matheus <math.lima.andrade@gmail.com>
2020-10-31 19:51:47 -0300

    projeto inicializado com o spring

---
+++ tech-start-pro/src/main/java/com/olist/techstartpro/TechStartProApplication.java
@@ -0,0 +1,13 @@
+package com.olist.techstartpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechStartProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechStartProApplication.class, args);
+	}
+
+}
---
+++ tech-start-pro/src/test/java/com/olist/techstartpro/TechStartProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olist.techstartpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechStartProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
