7d705293788fe9360ba465c4e987df253f9d556b
Matheus <math.lima.andrade@gmail.com>
2020-10-31 20:38:00 -0300

    added product class

---
+++ tech-start-pro/src/main/java/com/olist/techstartpro/domain/Product.java
@@ -0,0 +1,36 @@
+package com.olist.techstartpro.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonPOJOBuilder
+@EqualsAndHashCode(exclude = {"id"})
+@Data
+@Table
+@Builder
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private BigDecimal value;
+
+    @NotNull
+    private List<String> category;
+}
