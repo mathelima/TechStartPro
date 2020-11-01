4155ea1c5f44ba6dc61188d3ff72476cca2a94d4
Matheus <math.lima.andrade@gmail.com>
2020-10-31 22:31:26 -0300

    added ProductNotFoundException class

---
+++ tech-start-pro/src/main/java/com/olist/techstartpro/exception/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com.olist.techstartpro.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
